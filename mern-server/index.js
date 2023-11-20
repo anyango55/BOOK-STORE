fc1d14f153881df907472169e8e52bff90f4f7b6
anyango55 <anyangoc55@gmail.com>
2023-11-20 09:49:39 +0300

    setup

---
+++ mern-server/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// mongo db config
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://book-store:antonine142@cluster0.o56gpbt.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    // create a collection of documents
+    const booksCollections =  client.db("BookInventory").collection("books");
+
+    // insert a book to the db using POST method
+    app.post("./upload-book", async (req, res) => {
+      const data = req.body;
+      const result = await booksCollections.insertOne(data);
+      res.send(result);
+
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
