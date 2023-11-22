48c3438ca6d0d3af6e53d19ad920e5e84e38baf1
anyango55 <anyangoc55@gmail.com>
2023-11-22 11:22:13 +0300

    swiper carousel

--- mern-client/src/components/BookCards.jsx
+++ mern-client/src/components/BookCards.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+// import React from 'react'
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 
 const BookCards = ({headline, books}) => {
+
   return (
     <div className='my-16 px-4 lg:px-24'>
         <h2 className='text-5xl text-center font-bold text-black my-5'>{headline}</h2>
         {/* cards */}
         <div>
-            {/* use swiper.js> demos > responsive breakpoints */}
+        <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+         
+        {
+          books.map(book => <SwiperSlide key={book._id}>
+              <Link to='/'>
+                <div>
+                <img src={book.imageURL} alt=''/>
+                </div>
+                
+              </Link>
+
+              </SwiperSlide>)
+
+          }
+          </Swiper>
         </div>
     </div>
   )
