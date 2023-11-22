44e2c93fc48ab8230e1f3602b4350e6578b1638f
anyango55 <anyangoc55@gmail.com>
2023-11-22 11:53:27 +0300

    cart icon

--- mern-client/src/components/BookCards.jsx
+++ mern-client/src/components/BookCards.jsx
@@ -13,6 +13,7 @@ import 'swiper/css/pagination';
 // import required modules
 import { Pagination } from 'swiper/modules';
 import { Link } from 'react-router-dom';
+import { FaCartShopping } from 'react-icons/fa6'
 
 const BookCards = ({headline, books}) => {
 
@@ -47,9 +48,28 @@ const BookCards = ({headline, books}) => {
          
         {
           books.map(book => <SwiperSlide key={book._id}>
+            <div>
+              
+            </div>
               <Link to='/'>
-                <div>
+                <div className='relative'>
                 <img src={book.imageURL} alt=''/>
+                <div className='absolute top-3 right-3 bg-blue-600 hover:bg-black p-2 rounded'>
+                <FaCartShopping className='w-4 h-4 text-white'/>
+                </div>
+                
+                </div>
+                    
+                <div>
+                  <div>
+                    <h3>{book.bookTitle}</h3>
+                    <p>{book.authorName}</p>
+                  </div>
+
+                  <div>
+                    <p>$10.00</p>
+                  </div>
+
                 </div>
                 
               </Link>
